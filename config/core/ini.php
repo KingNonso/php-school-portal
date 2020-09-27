6a5b8be36b54e164f2f176d8e503d2455c4a549e
Chinonso Ani <achinonso@gmail.com>
2020-09-27 20:46:37 +0100

    updated on mac

--- config/core/ini.php
+++ config/core/ini.php
@@ -5,7 +5,7 @@ $GLOBALS['config'] = array(
 	'mysql' => array(
 			'host' => '127.0.0.1',
 			'username' => 'root',
-			'password' => '',
+			'password' => 'password',
 			'db' => 'school_portal'
 		),
 	'remember' => array(
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     //use an autoloader
     require 'config/core/ini.php';
 
-    define('URL', 'http://localhost/code/school_portal/');
+    define('URL', 'http://localhost/school_portal/');
     define('UPLOAD_PATH', __DIR__.'\public\uploads\\');
     define('TEST_INC', __DIR__.'\views\\');
 
